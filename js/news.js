d84645ea9ceb2096fde2cdac580e650c44523a96
ikoma <maimai_na_srtci@yahoo.co.jp>
2023-06-18 17:52:31 +0900

    ファイル追加

---
+++ js/news.js
@@ -0,0 +1,13 @@
+// document.getElementById("newscherry").onclick = function() {
+  // document.getElementById("newscherrycoment").style.display = "none";
+// }
+
+// document.getElementById("newscherrycoment").style.display = "none";
+document.getElementById("newscherrycoment").style.display = "none";
+
+const cherryComent = document.getElementById("newscherry");
+cherryComent.addEventListener("click", () => {
+  document.getElementById("newscherrycoment").style.display = "";
+};
+
+// ↑　反映されない　「2023.6.20　サクランボ狩りの予約を開始しました。」にクリックすると、「大変お待たせ致しました。～」が表示されるようにしたい（スライド？）
